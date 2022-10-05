51171a838a05e224066bffccfdbb4f285b4cdcbe
Juanan Pereira <juanan@ikasten.io>
2022-10-05 12:58:19 +0200

    Initial import

---
+++ js/main.js
@@ -0,0 +1,43 @@
+import {datubasea} from './datubasea.js'
+
+
+let indizea = 0
+const URLBASE = 'https://covers.openlibrary.org/b/id/'
+let izenburua  = document.getElementById('izenburua');
+let irudia = document.getElementById('irudia')
+let egilea = document.getElementById('egilea')
+let isbn = document.getElementById('isbn')
+let aurrera = document.getElementById('aurrera')
+let atzera = document.getElementById('atzera')
+
+function eremuakBete(){
+
+    izenburua.value = datubasea[indizea].izenburua
+    data.value = datubasea[indizea].data
+    egilea.value = datubasea[indizea].egilea
+    isbn.value = datubasea[indizea].isbn
+    irudia.src = URLBASE + datubasea[indizea].filename 
+
+}
+
+
+function kargatu(){
+
+    eremuakBete()
+
+    aurrera.addEventListener('click', (event) => {
+        if (indizea < datubasea.length-1)
+            indizea++
+        eremuakBete()
+    })
+    atzera.addEventListener('click', (event) => {
+        if (indizea > 0)
+            indizea--
+        eremuakBete()
+    })
+
+
+}
+
+window.onload = kargatu;
+
