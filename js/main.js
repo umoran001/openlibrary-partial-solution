8fd5a0e99fe9c2bb00e35b61d14458e14ffd51bc
umoran001 <umoran001@ikasle.ehu.eus>
2022-10-26 11:39:14 +0200

    proiektua igo

--- js/main.js
+++ js/main.js
@@ -9,7 +9,8 @@ let egilea = document.getElementById('egilea')
 let isbn = document.getElementById('isbn')
 let aurrera = document.getElementById('aurrera')
 let atzera = document.getElementById('atzera')
-
+let bilatu = document.getElementById('bilatu')
+let kop= document.getElementById('libKop')
 function eremuakBete(){
 
     izenburua.value = datubasea[indizea].izenburua
@@ -17,11 +18,25 @@ function eremuakBete(){
     egilea.value = datubasea[indizea].egilea
     isbn.value = datubasea[indizea].isbn
     irudia.src = URLBASE + datubasea[indizea].filename 
+    kop.innerText=datubasea.length
 
 }
 
+function convert(is){
+    console.log(is)
+    let gakoa=Object.keys(is)
+    return {
+     "isbn": isbn.value,
+    "egilea": is[gakoa].details.authors['0'].name,
+    "data": is[gakoa].details.publish_date,
+    "izenburua": is[gakoa].details.title,
+    "filename": is[gakoa].thumbnail_url.replace('-S','-M').replace(URLBASE,'')
+    }
+    
+}
 
-function kargatu(){
+
+ function kargatu(){
 
     eremuakBete()
 
@@ -35,6 +50,19 @@ function kargatu(){
             indizea--
         eremuakBete()
     })
+    bilatu.onclick= async (event) => {
+        let i= (datubasea.findIndex(is => is.isbn==isbn.value))
+        if(i!=-1){
+            indizea=i;
+            eremuakBete()        
+        }else{
+            let objektua=await fetch("http://openlibrary.org/api/books?bibkeys=ISBN:"+isbn.value+"&format=json&jscmd=details").then(r=> r.json())
+            datubasea.push(convert(objektua));
+            console.log(datubasea)
+            indizea=datubasea.length-1;
+            eremuakBete()
+        }
+    }
 
 
 }
